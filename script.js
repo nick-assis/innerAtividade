b7da8c5d32f4b1b938d4827f0bf7d13352449f03
Nícolas de Assis <nicolasassis@ciandt.com>
2024-04-24 20:10:37 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let titulo = document.getElementById("titulo");
+titulo.innerText = "Novo texto";
+
+let link = document.querySelector('a');
+link.innetText = 'Sute Proz';
+
+let listaNaoOrdenada = document.querySelector('ul');
+listaNaoOrdenada.ineerHTML = `
+<li>React</li>
+<li>Vue</li>
+<li>Angular</li>
+`
+
+let listaOrdenada = document.querySelector('ol');
+listaOrdenada.innerHTMLHTML = `
+<li><a href="https://azure.microsoft.com/pt-br/resources/cloud-computing-dictionary/what-is-azure"/>Azure</li>
+<li><a href="https://aws.amazon.com/pt/free/?gclid=CjwKCAjw26KxBhBDEiwAu6KXt4atB6Iv_oBX8jN4nIB3hyNAN1YWWSYl4VhnFb3V0XBKiibB-Cf2RxoCJMkQAvD_BwE&trk=2ee11bb2-bc40-4546-9852-2c4ad8e8f646&sc_channel=ps&ef_id=CjwKCAjw26KxBhBDEiwAu6KXt4atB6Iv_oBX8jN4nIB3hyNAN1YWWSYl4VhnFb3V0XBKiibB-Cf2RxoCJMkQAvD_BwE:G:s&s_kwcid=AL!4422!3!561843094929!e!!g!!aws!15278604629!130587771740&all-free-tier.sort-by=item.additionalFields.SortRank&all-free-tier.sort-order=asc&awsf.Free%20Tier%20Types=*all&awsf.Free%20Tier%20Categories=*all"/>AWS</li>
+<li><a href="https://magalu.cloud/"/>Magalu Clod</li>
+`
